2ff2b33dfea39df9ca890862daeb7b839e6a9697
joao.abrantes <jpa@cesar.org.br>
2022-03-22 17:58:00 -0300

    v1.1 - Update hour in real time

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+function main() {
+    setInterval(load, 1000);
+}
+
 function load() {
     let msg = window.document.querySelector('#msg');
     let img = window.document.querySelector('#pic');
     let data = new Date();
-    let hora = data.getHours();
-    let min = data.getMinutes();
-    msg.innerHTML = `<p>Agora são ${hora}h${min}</p>`;
+    let hora = formatNumberToString(data.getHours());
+    let min = formatNumberToString(data.getMinutes());
+    let sec = formatNumberToString(data.getSeconds());
+
+    msg.innerHTML = `<p>Agora são ${hora}:${min}:${sec}</p>`;
     if (hora < 12) {
         msg.innerHTML += `<p>Bom dia!</p>`;
-        img.src = 'img/manha.jpg'
+        img.src = 'img/manha.jpg';
     } else if (hora < 18) {
         msg.innerHTML += `<p>Boa tarde!</p?`;
-        img.src = 'img/tarde.jpg'
+        img.src = 'img/tarde.jpg';
     } else {
         msg.innerHTML += `<p>Boa noite!</p>`;
-        img.src = 'img/noite.jpg'
+        img.src = 'img/noite.jpg';
+    }
+}
+
+function formatNumberToString(num) {
+    if (num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
     }
 }
