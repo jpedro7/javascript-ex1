899af3a47eed97dec9c0058c825b5d6af2819605
jpedro7 <joaopedroabrantes123@gmail.com>
2022-03-22 23:59:27 -0300

    v1.4 - add lo fi music

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 function main() {
     load();
+    
+    setInterval(load, 1000);
+}
 
-    setInterval(load, 100);
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+    document.querySelector('#music-frame').setAttribute('hidden', true);
+    var player = new YT.Player('music-frame', {
+        videoId: '-9gEgshJUuY',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+    event.target.setVolume(20);
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
 }
 
 function load() {
+    let data = new Date();
     let msg = window.document.querySelector('#msg');
     let img = window.document.querySelector('#pic');
-    let data = new Date();
     let hora = formatNumberToString(data.getHours());
     let min = formatNumberToString(data.getMinutes());
     let sec = formatNumberToString(data.getSeconds());
 
-    msg.innerHTML = `<p>Agora são ${hora}:${min}:${sec}</p>`;
+    msg.innerHTML = `${hora}:${min}:${sec}`;
     if (hora < 12) {
         msg.innerHTML += `<p>Bom dia!</p>`;
         img.src = 'img/manha.jpg';
