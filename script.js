32378f219eaa79c0afd6d0ebb076488515519358
jpedro7 <joaopedroabrantes123@gmail.com>
2022-03-24 11:50:57 -0300

    v1.6 - Remove music

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
 function main() {
     load();
     
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    
     setInterval(load, 1000);
-}
-
-function onYouTubeIframeAPIReady() {
-    document.querySelector('#music-frame').setAttribute('hidden', true);
-    var player = new YT.Player('music-frame', {
-        videoId: '-9gEgshJUuY',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-    event.target.setVolume(20);
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-
-function stopVideo() {
-    player.stopVideo();
 }
 
 function load() {
